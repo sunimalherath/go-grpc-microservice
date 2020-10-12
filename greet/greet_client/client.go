86121841a36246568015389f352b6d71dfe5ab99
sunimalherath <sunimaldv@gmail.com>
2020-10-12 12:38:09 +1100

    implement client and server

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/sunimalherath/grpc-go/greet/greetpb"
 	"google.golang.org/grpc"
 	"log"
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// 1. Create a connection to server
-	cc, err := grpc.Dial("localhost:500051", grpc.WithInsecure())
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
@@ -18,5 +18,21 @@ func main() {
 
 	// 2. Crete the client
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Client created: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "John",
+			LastName:  "Doe",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", res.Result)
 }
