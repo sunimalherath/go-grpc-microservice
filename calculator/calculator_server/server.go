a0d8c53e28b558dd47e0daf05fa94398d0ed8bfe
sunimalherath <sunimaldv@gmail.com>
2020-10-24 17:30:58 +1100

    implementing errors

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
+	"math"
 	"net"
 
 	"github.com/sunimalherath/grpc-go/calculator/calculatorpb"
 	"github.com/sunimalherath/grpc-go/calculator/sumpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct{}
@@ -76,4 +80,17 @@ func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAvera
 		total += int(req.GetNumber())
 		count++
 	}
+}
+
+func (*server) SquareRoot(ctx context.Context, req *calculatorpb.SquareRootRequest) (*calculatorpb.SquareRootResponse, error) {
+	number := req.GetNumber()
+	if number < 0 {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Received a negative number: %v", number),
+		)
+	}
+	return &calculatorpb.SquareRootResponse{
+		NumberRoot: math.Sqrt(float64(number)),
+	}, nil
 }
